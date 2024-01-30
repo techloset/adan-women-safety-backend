a098e574e2dc52f31d24929f06f5cf87e500c6a2
Adan <adanatif60@gmail.com>
2024-01-30 20:41:46 +0500

    Fixed Error

--- src/app/api/group/school/getMessage/route.ts
+++ src/app/api/group/school/getMessage/route.ts
@@ -15,7 +15,7 @@ export const GET = async (req: Request) => {
     //   .from(yourTable)
     //   .where(eq(yourTable.school, "test"))
     //   .all();
-    // return NextResponse.json({ messagesWithUsers }, { status: 200 });
+    return NextResponse.json({ hello: "sc" }, { status: 200 });
   } catch (error: any) {
     console.log("error", error);
     return NextResponse.json({ error: error.message }, { status: 500 }); // Return an error response
